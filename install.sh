6a8d618676abffa2fe190f19f7ca3971227857c9
Xiangdong Zeng <xdzeng96@gmail.com>
2018-04-12 19:11:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
-pwd
-ls
+WORKING_DIR=$PWD
+INCLUDE_DIR=$WORKING_DIR/ising/include
 
+mkdir -p $INCLUDE_DIR/argagg
+cd $INCLUDE_DIR/argagg
 wget https://raw.githubusercontent.com/vietjtnguyen/argagg/master/include/argagg/argagg.hpp
+
+mkdir -p $WORKING_DIR/tmp
+cd $WORKING_DIR/tmp
+git clone --depth=1 https://github.com/Tencent/rapidjson.git
+cp -r ./rapidjson/include $INCLUDE_DIR
+
+cd $WORKING_DIR
